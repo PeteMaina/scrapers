d8f1065ce57d2788360bc50b4b9f8139ef842da3
Pete <peterwahomemaina003@gmail.com>
2025-02-20 18:32:49 +0300

    commit

---
+++ Linkedin.py
@@ -0,0 +1,9 @@
+'''
+Feel free to email me at peterwahomemaina003@gmail.com
+
+This code is helpful to Techies and anyone in general
+this is a web scraper in python
+It scrapes and searches for the desired jobs through LinkedIn and acts as linked in premium
+
+you can run this code on a python environment or use jupiter notebooks
+'''
